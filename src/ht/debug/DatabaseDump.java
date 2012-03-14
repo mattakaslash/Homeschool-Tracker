b2b4d9bd116a68a0597514e93b91953837556a2b
matthew.rinehart <matthew.rinehart@navy.mil>
2012-03-14 16:19:03 -0400

    adding debug utility

---
+++ src/ht/debug/DatabaseDump.java
@@ -0,0 +1,129 @@
+package ht.debug;
+
+import ht.model.Assignment;
+import ht.model.Category;
+import ht.model.Concept;
+import ht.model.ConfigurationEntry;
+import ht.model.CurriculumLogEntry;
+import ht.model.Day;
+import ht.model.FieldTrip;
+import ht.model.JournalEntry;
+import ht.model.PlannerEntry;
+import ht.model.ReadingListEntry;
+import ht.model.ReportCardEntry;
+import ht.model.Student;
+import ht.model.Subject;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+public class DatabaseDump {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EntityManager em = Persistence.createEntityManagerFactory("records").createEntityManager();
+		Class<?>[] entities = { Assignment.class, Category.class, Concept.class, ConfigurationEntry.class,
+				CurriculumLogEntry.class, Day.class, FieldTrip.class, JournalEntry.class, PlannerEntry.class,
+				ReadingListEntry.class, ReportCardEntry.class, Student.class, Subject.class };
+
+		System.out.println("Assignment");
+		System.out.println("---------------------------------------------------------------------------------------------");
+		for (Object o : em.createQuery("select o from Assignment o").getResultList()) {
+			System.out.println(((Assignment) o));
+		}
+		System.out.println();
+
+		System.out.println("Category");
+		System.out.println("---------------------------------------------------------------------------------------------");
+		for (Object o : em.createQuery("select o from Category o").getResultList()) {
+			System.out.println(((Category) o));
+		}
+		System.out.println();
+
+		System.out.println("Concept");
+		System.out.println("---------------------------------------------------------------------------------------------");
+		for (Object o : em.createQuery("select o from Concept o").getResultList()) {
+			System.out.println(((Concept) o));
+		}
+		System.out.println();
+
+		System.out.println("ConfigurationEntry");
+		System.out.println("---------------------------------------------------------------------------------------------");
+		for (Object o : em.createQuery("select o from ConfigurationEntry o").getResultList()) {
+			System.out.println(((ConfigurationEntry) o));
+		}
+		System.out.println();
+
+		System.out.println("CurriculumLogEntry");
+		System.out.println("---------------------------------------------------------------------------------------------");
+		for (Object o : em.createQuery("select o from CurriculumLogEntry o").getResultList()) {
+			System.out.println(((CurriculumLogEntry) o));
+		}
+		System.out.println();
+
+		System.out.println("Category");
+		System.out.println("---------------------------------------------------------------------------------------------");
+		for (Object o : em.createQuery("select o from Category o").getResultList()) {
+			System.out.println(((Category) o));
+		}
+		System.out.println();
+
+		System.out.println("Day");
+		System.out.println("---------------------------------------------------------------------------------------------");
+		for (Object o : em.createQuery("select o from Day o").getResultList()) {
+			System.out.println(((Day) o));
+		}
+		System.out.println();
+
+		System.out.println("FieldTrip");
+		System.out.println("---------------------------------------------------------------------------------------------");
+		for (Object o : em.createQuery("select o from FieldTrip o").getResultList()) {
+			System.out.println(((FieldTrip) o));
+		}
+		System.out.println();
+
+		System.out.println("JournalEntry");
+		System.out.println("---------------------------------------------------------------------------------------------");
+		for (Object o : em.createQuery("select o from JournalEntry o").getResultList()) {
+			System.out.println(((JournalEntry) o));
+		}
+		System.out.println();
+
+		System.out.println("PlannerEntry");
+		System.out.println("---------------------------------------------------------------------------------------------");
+		for (Object o : em.createQuery("select o from PlannerEntry o").getResultList()) {
+			System.out.println(((PlannerEntry) o));
+		}
+		System.out.println();
+
+		System.out.println("ReadingListEntry");
+		System.out.println("---------------------------------------------------------------------------------------------");
+		for (Object o : em.createQuery("select o from ReadingListEntry o").getResultList()) {
+			System.out.println(((ReadingListEntry) o));
+		}
+		System.out.println();
+
+		System.out.println("ReportCardEntry");
+		System.out.println("---------------------------------------------------------------------------------------------");
+		for (Object o : em.createQuery("select o from ReportCardEntry o").getResultList()) {
+			System.out.println(((ReportCardEntry) o));
+		}
+		System.out.println();
+
+		System.out.println("Student");
+		System.out.println("---------------------------------------------------------------------------------------------");
+		for (Object o : em.createQuery("select o from Student o").getResultList()) {
+			System.out.println(((Student) o));
+		}
+		System.out.println();
+
+		System.out.println("Subject");
+		System.out.println("---------------------------------------------------------------------------------------------");
+		for (Object o : em.createQuery("select o from Subject o").getResultList()) {
+			System.out.println(((Subject) o));
+		}
+		System.out.println();
+	}
+}
