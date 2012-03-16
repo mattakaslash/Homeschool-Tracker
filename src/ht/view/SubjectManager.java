fa3070a0d254533456327854a0d90bc4321fd1dc
Matthew Rinehart <gomamon2k@yahoo.com>
2012-03-16 14:20:27 -0400

    additional UI controls

--- src/ht/view/SubjectManager.java
+++ src/ht/view/SubjectManager.java
@@ -9,8 +9,11 @@ import java.awt.Frame;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
@@ -30,7 +33,10 @@ public class SubjectManager extends JDialog {
 	private static final long serialVersionUID = -2470705175032575109L;
 	private JTree jTreeSubjects;
 	private JScrollPane jScrollPaneSubjects;
+	private JPanel jPanelControls;
 	private static final TreeModel subjectsTreeModel = new SubjectsTreeModel();
+	private JTextField jTextFieldSubjectTitle;
+	private JButton jButtonSaveAll;
 
 	/**
 	 * Creates a default, parent-less dialog.
@@ -56,13 +62,39 @@ public class SubjectManager extends JDialog {
 		setModal(true);
 		setForeground(Color.black);
 		add(getJScrollPaneSubjects(), BorderLayout.CENTER);
+		add(getJPanelControls(), BorderLayout.SOUTH);
 		addWindowListener(new WindowAdapter() {
 	
 			public void windowOpened(WindowEvent event) {
 				windowWindowOpened(event);
 			}
 		});
-		setSize(320, 240);
+		setSize(480, 320);
+	}
+
+	private JButton getJButtonSaveAll() {
+		if (jButtonSaveAll == null) {
+			jButtonSaveAll = new JButton();
+			jButtonSaveAll.setText("Save All");
+		}
+		return jButtonSaveAll;
+	}
+
+	private JTextField getJTextFieldSubjectTitle() {
+		if (jTextFieldSubjectTitle == null) {
+			jTextFieldSubjectTitle = new JTextField();
+		}
+		return jTextFieldSubjectTitle;
+	}
+
+	private JPanel getJPanelControls() {
+		if (jPanelControls == null) {
+			jPanelControls = new JPanel();
+			jPanelControls.setLayout(new BorderLayout());
+			jPanelControls.add(getJTextFieldSubjectTitle(), BorderLayout.CENTER);
+			jPanelControls.add(getJButtonSaveAll(), BorderLayout.EAST);
+		}
+		return jPanelControls;
 	}
 
 	private JScrollPane getJScrollPaneSubjects() {
